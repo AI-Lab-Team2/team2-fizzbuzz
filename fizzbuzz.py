30bddbb2a14ebe125d29a86379e57a052f6d1001
“lojino” <dev.jinhochoi@gmail.com>
2024-10-25 17:09:59 +0900

    feat&fix: print fizzbuzz text in main and fix if statement

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,7 +8,11 @@ def fizzbuzz(num: int) -> str:
     input: int
     output: str
     '''
-    if num == 3 or num == 5:
+    if num%3 == 0 or num%5 == 0:
         return 'fizz'*(num%3==0) + 'buzz'*(num%5==0)
     else:
         return f"{num}"
+    
+if __name__ == "__main__":
+    for i in range(1,16):
+        print(fizzbuzz(i))
