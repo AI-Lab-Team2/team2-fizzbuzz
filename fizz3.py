4c1e2171aba74ee2a60f2f09490a44e339c9ace8
jinibizsite <jinibizsite@gmail.com>
2024-10-25 17:16:23 +0900

    feat:Add 5

--- fizz3.py
+++ fizz3.py
@@ -1,5 +1,9 @@
 for num in range(1, 15+1)
     if num%15 == 0 :
-        print('fizzbuzz')
+        print('buzz')
+    if num%5 == 0:
+        print('buzz')
     if num%3 == 0 :
         print('fizz')
+
+
